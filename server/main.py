29b4213d1aa60d4a455bc57c1635ec738a1c0b2d
jlee7 <joseflee7@gmail.com>
2018-03-05 21:56:20 +0100

    merge?

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 
-import game_model
+#import game_model
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
