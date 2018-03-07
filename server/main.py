07362203310e82700f886f4dd6fb83e9ac337789
jlee7 <joseflee7@gmail.com>
2018-03-07 23:55:56 +0100

    JSON in HTML. Unified functions in main.py.

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
@@ -6,49 +7,38 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-# Return index.html
+# Return index.html----------------------
 @app.route("/")
 def index():
     return render_template('index.html')
-
-# Receiving events
-# Test events --- Connection routine ------------------------
-@socketio.on('message_from_client')
-def handle_my_custom_event(json):
-    #print('received json: ' + str(json))
-    print('Message from client: ' + json["data"])
-    emit('message_to_client', {'data': 'SOME_OTHER_EVENT'})
-
-@socketio.on('another_message_from_client')
-def handle_my_custom_event(json):
-    #print('received json: ' + str(json))
-    print('Another message from client: ' + json["data"])
-
-
 # ---------------------------------------
 
-# Receivable events from client to server
-@socketio.on('name_of_client')
-def handle_my_custom_event(json):
-    pass
-
-@socketio.on('')
-def handle_my_custom_event(json):
-    pass
-
-@socketio.on('')
-def handle_my_custom_event(json):
-    pass
+# Receivable event from client to server
+@socketio.on('message_from_client') #  Event name is crucial.
+def handle_my_custom_event(json_data):
+    """Use this function to receive events from the client.
+       A JSON is always sent along and it hold all necessary data."""
+    print("Got message from client:", json.loads(json_data)["context"])
+    emit_event("message_to_client", json.loads(json_data)["data"])
+    emit_event_to_all_clients("message_to_client", json.loads(json_data)["data"])
 # ----------------------------------------
 
-# Emit events to clients
-def emit_event(event_name, json):
-    emit(event_name, json)
-    print("Sending to clients",
-event_name)
+# Emit events to one client as response
+def emit_event(event_name, json_data):
+    """Use this function to emit events to the client which sent data.
+       A JSON is always sent along and it hold all necessary data.
+       This is not a broadcasting to all clients!"""
+    emit(event_name, json_data)
+    print("Sending to one client as response: ",event_name)
 # ----------------------
 
-
+# Emit events to all clients as broadcast
+def emit_event_to_all_clients(event_name, json_data):
+    """Use this function to emit events to all clients as broadcast.
+       A JSON is always sent along and it hold all necessary data."""
+    socketio.emit(event_name, json_data)
+    print("Sending to all clients: ",event_name)
+# ---------------------------------------
 
 # Boilerplate starts SocketIO instead of standard flask.
 if __name__ == '__main__':
@@ -61,4 +51,7 @@ send() and emit() is used in client
 context for answering.
 socketio.send() and socketio.emit() is
 used when server starts communication.
+
+json.loads() --> string to json
+json.dumps() --> json to string
 """
--- server/model.py
+++ server/model.py
@@ -1,16 +1,12 @@
-"""
-Players
-Player
-"""
 
-class Players:
+
+
+class Session:
     def __init__(self):
-        self.list_of_players = list()
+        self.players = list()
+
+
 
 class Player:
     def __init__(self):
-        self.name = name
-        self.is_ready = False
-        self.is_host = False
-        self.points = 0
-        self.games_won = 0
+        self.name = str()
