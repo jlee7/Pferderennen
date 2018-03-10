6f096c784ddfd75f5e2ad058c5624e955ea7f25c
jlee7 <joseflee7@gmail.com>
2018-03-10 11:34:05 +0100

    Removed Game class, added MessageHandler and SessionHandler.

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-import json
+
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
@@ -8,33 +8,40 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-# Create Session instance from model.py
-game_session = model.Session()
+# Create instances from classes in model.py
+session_handler = model.SessionHandler()
+message_handler = model.MessageHandler()
 
-# Return index.html----------------------
+# Return index.html
 @app.route("/")
 def index():
+    """Return HTML"""
     return render_template('index.html')
-# ---------------------------------------
 
-# Receivable event from client to server
+## Receivable event from client to server
+# Reserved events
+@socketio.on('connect')
+def test_connect():
+    """Gets fired on any client-server connection"""
+    session_handler.add_client(request.sid)
+    print("Connected:", request.sid)
+
+@socketio.on('disconnect')
+def test_disconnect():
+    """Gets fired on any client-server disconnect"""
+    session_handler.remove_client(request.sid)
+    print('Disconnected:', request.sid)
+
+# Custom events
 @socketio.on('message_from_client') #  Event name is crucial.
 def handle_my_custom_event(json_data):
     """Use this function to receive events from the client.
        A JSON is always sent along and it hold all necessary data."""
-    #print("Got RAW message from client:",json_data)
-    #print("Got message from client:", json.loads(json_data))
-    print("MAIN: Request.SID:", request.sid)
-    if json.loads(json_data)["type"] == "echo":
-        print("MAIN: Event Type:", json_loads(json_data)["type"])
-    elif json.loads(json_data)["type"] == "game_start":
-        game_session.add_player(request.sid, json_data)
+    message_handler.process_message(request.sid, json_data)
+    print("MAIN: Received a message and forwarding to MessageHandler.")
 
-    #emit_event("message_to_client", json_data)
-    #emit_event_to_all_clients("message_to_client", json_data)
-# ----------------------------------------
 
-# Emit events to one client as response
+## Emit events to one client as response
 def emit_event(event_name, json_data):
     """Use this function to emit events to the client which sent data.
        A JSON is always sent along and it hold all necessary data.
@@ -42,15 +49,13 @@ def emit_event(event_name, json_data):
     emit(event_name, json_data)
     print("MAIN: Sending to one client as response: ",event_name)
 
-# ----------------------
 
-# Emit events to all clients as broadcast
+## Emit events to all clients as broadcast
 def emit_event_to_all_clients(event_name, json_data):
     """Use this function to emit events to all clients as broadcast.
        A JSON is always sent along and it hold all necessary data."""
     print("MAIN: Sending to all clients: ",event_name)
 
-# ---------------------------------------
 
 # Boilerplate starts SocketIO instead of standard flask.
 if __name__ == '__main__':
@@ -66,4 +71,6 @@ used when server starts communication.
 
 json.loads() --> string to json
 json.dumps() --> json to string
+
+request.sid --> unique session ID for each client connection
 """
--- server/model.py
+++ server/model.py
@@ -1,26 +1,55 @@
+""" This file contains all classes for the game. """
+
+import json
+
+class Player:
+    def __init__(self, session_id, name):
+        self.name = str(name)
+        self.session_id = session_id
 
 
 
-class Session:
+class MessageHandler:
+    """ The MessageHandler receives all incoming messages
+        and processes them."""
+    def __init__(self):
+        pass
+
+    def process_message(self, session_id, json_data):
+        #print("Got RAW message from client:",json_data)
+        #print("Got message from client:", json.loads(json_data))
+        print("MAIN: Request.SID:", session_id)
+        if json.loads(json_data)["type"] == "echo":
+            print("MAIN: Event Type:", json_loads(json_data)["type"])
+        #elif json.loads(json_data)["type"] == "game_start":
+            #game_session.add_player(session_id, json_data)
+
+        #emit_event("message_to_client", json_data)
+        #emit_event_to_all_clients("message_to_client", json_data)
+
+
+class SessionHandler:
+    """ The SessionHandler receives all Session IDs (request.sid)
+        and knows about all connected and disconnected clients."""
     def __init__(self):
         self.players = list()
 
-    def check_if_sid_is_free(self, SID):
-        if SID in [x[0] for x in self.players]:
+    def check_if_already_exists(self, session_id):
+        print([x for x in self.players])
+        if session_id in [x for x in self.players]:
             print("MODEL: Already taken.")
             return False
         else:
             print("MODEL: Can be added.")
             return True
 
-    def add_player(self, SID, player_name):
-        if self.check_if_sid_is_free(SID):
-            self.players.append((SID, player_name))
+    def add_client(self, session_id):
+        if self.check_if_already_exists(session_id):
+            self.players.append(session_id)
             print("MODEL: Player added to Session.")
             print("MODEL:", self.players)
 
-
-
-class Player:
-    def __init__(self):
-        self.name = str()
+    def remove_client(self, session_id):
+        self.players.remove(session_id)
+        print("MODEL: Player removed from Session.")
+        print("MODEL:", self.players)
