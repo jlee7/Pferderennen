b122b829416fc84122295d69a541c76440d95067
jlee7 <joseflee7@gmail.com>
2018-03-10 12:58:18 +0100

    Deleted SessionHandler and MessageHandler. Created EventManager. Blocked redundant registration. Player gets deleted when disconnect.

--- server/main.py
+++ server/main.py
@@ -1,16 +1,14 @@
-
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
 import model
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'secret!' # super secret
 socketio = SocketIO(app)
 
 # Create instances from classes in model.py
-session_handler = model.SessionHandler()
-message_handler = model.MessageHandler()
+event_manager = model.EventManager()
 
 # Return index.html
 @app.route("/")
@@ -23,22 +21,25 @@ def index():
 @socketio.on('connect')
 def test_connect():
     """Gets fired on any client-server connection"""
-    session_handler.add_client(request.sid)
-    print("Connected:", request.sid)
+    print("MAIN: Connected:", request.sid)
+    event_manager.process_message(request.sid, """{"type": "connect"}""")
+
 
 @socketio.on('disconnect')
 def test_disconnect():
     """Gets fired on any client-server disconnect"""
-    session_handler.remove_client(request.sid)
-    print('Disconnected:', request.sid)
+    print('MAIN: Disconnected:', request.sid)
+    event_manager.process_message(request.sid, """{"type": "disconnect"}""")
+
 
 # Custom events
 @socketio.on('message_from_client') #  Event name is crucial.
 def handle_my_custom_event(json_data):
     """Use this function to receive events from the client.
        A JSON is always sent along and it hold all necessary data."""
-    message_handler.process_message(request.sid, json_data)
     print("MAIN: Received a message and forwarding to MessageHandler.")
+    event_manager.process_message(request.sid, json_data)
+
 
 
 ## Emit events to one client as response
@@ -46,8 +47,9 @@ def emit_event(event_name, json_data):
     """Use this function to emit events to the client which sent data.
        A JSON is always sent along and it hold all necessary data.
        This is not a broadcasting to all clients!"""
-    emit(event_name, json_data)
     print("MAIN: Sending to one client as response: ",event_name)
+    emit(event_name, json_data)
+
 
 
 ## Emit events to all clients as broadcast
@@ -64,10 +66,8 @@ if __name__ == '__main__':
 
 """
 Notes:
-send() and emit() is used in client
-context for answering.
-socketio.send() and socketio.emit() is
-used when server starts communication.
+send() and emit() is used in client context for answering.
+socketio.send() and socketio.emit() is used when server starts communication.
 
 json.loads() --> string to json
 json.dumps() --> json to string
--- server/model.py
+++ server/model.py
@@ -2,54 +2,53 @@
 
 import json
 
+
 class Player:
     def __init__(self, session_id, name):
         self.name = str(name)
         self.session_id = session_id
 
 
-
-class MessageHandler:
-    """ The MessageHandler receives all incoming messages
+class EventManager:
+    """ The EventManager receives all incoming messages
         and processes them."""
     def __init__(self):
-        pass
+        self.players = dict()
 
-    def process_message(self, session_id, json_data):
-        #print("Got RAW message from client:",json_data)
-        #print("Got message from client:", json.loads(json_data))
-        print("MAIN: Request.SID:", session_id)
-        if json.loads(json_data)["type"] == "echo":
-            print("MAIN: Event Type:", json_loads(json_data)["type"])
-        #elif json.loads(json_data)["type"] == "game_start":
-            #game_session.add_player(session_id, json_data)
+    def list_players(self):
+        """Prints all players to console."""
+        for player in self.players:
+            print(player)
 
-        #emit_event("message_to_client", json_data)
-        #emit_event_to_all_clients("message_to_client", json_data)
-
-
-class SessionHandler:
-    """ The SessionHandler receives all Session IDs (request.sid)
-        and knows about all connected and disconnected clients."""
-    def __init__(self):
-        self.players = list()
-
-    def check_if_already_exists(self, session_id):
-        print([x for x in self.players])
-        if session_id in [x for x in self.players]:
-            print("MODEL: Already taken.")
-            return False
-        else:
-            print("MODEL: Can be added.")
+    def check_if_session_id_exists(self, session_id):
+        """Returns True if SID already exists."""
+        if session_id in self.players:
+            print("Session ID exists:", session_id)
             return True
 
-    def add_client(self, session_id):
-        if self.check_if_already_exists(session_id):
-            self.players.append(session_id)
-            print("MODEL: Player added to Session.")
-            print("MODEL:", self.players)
+    def process_message(self, session_id, json_data):
+        """Processes each message type defined in "type" in JSON."""
+        print("MODEL: Event Type:", json.loads(json_data)["type"])
 
-    def remove_client(self, session_id):
-        self.players.remove(session_id)
-        print("MODEL: Player removed from Session.")
-        print("MODEL:", self.players)
+        if json.loads(json_data)["type"] == "echo":
+            print("MODEL:", session_id, json_data)
+
+        elif json.loads(json_data)["type"] == "connect":
+            pass
+
+        elif json.loads(json_data)["type"] == "disconnect":
+            print("MODEL:", session_id, json_data)
+            if session_id in self.players:
+                del(self.players[session_id])
+
+        elif json.loads(json_data)["type"] == "game_start":
+            print("MODEL:", session_id, json_data)
+            if self.check_if_session_id_exists(session_id):
+                print("MODEL: Cannot add player because SID already exists.")
+                self.list_players()
+            else:
+                self.players[session_id] = Player(session_id, json.loads(json_data)["data"]["name"])
+                self.list_players()
+                print(self.players)
+        #emit_event("message_to_client", json_data)
+        #emit_event_to_all_clients("message_to_client", json_data)
