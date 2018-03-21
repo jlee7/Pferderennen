b419b45e27657486a0d0f7454e4d20f806498fc1
jlee7 <joseflee7@gmail.com>
2018-03-21 23:39:47 +0100

    Removed event manager and integrated functionality into main Flask app. Fixed disconnect event when no player is in session.

--- game_session.py
+++ game_session.py
@@ -18,12 +18,26 @@ class GameSession:
             instances, the session ID of each instance must be checked.
             If match, the index if the instance is used to delete it from
             the list. """
-        for index_of_player_class, player in enumerate(self.players):
-            if player.sid == session_id:
-                break
-            else:
-                index_of_player_class = -1
-        del self.players[index_of_player_class]
+
+        # Check if self.player is empty already
+        if len(self.players) > 0:
+            # Get index to remove by index from list
+            for index_of_player_class, player in enumerate(self.players):
+                print("SESSION: remove_player: IF", index_of_player_class, player)
+                # Match session_id with the session ID from each player in list
+                if player.sid == session_id:
+                    index_of_player_class = self.players.index(player)
+                    print(index_of_player_class)
+                    #del self.players[index_of_player_class]
+                else:
+                    print("SESSION: remove_player: ELSE", index_of_player_class)
+                    index_of_player_class = -1
+                    print(index_of_player_class)
+
+                # Delete the player by index
+                del self.players[index_of_player_class]
+        else:
+            print("List of players is empty.")
 
     def list_players(self):
         """ Can be removed after finishing. """
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
+import json
 
-import eventmanager
+import game_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!' # super secret
 socketio = SocketIO(app)
 
-# Create instances from classes in model.py
-event_manager = eventmanager.EventManager()
+game_session = game_session.GameSession()
 
 # Return index.html
 @app.route("/")
@@ -16,31 +16,27 @@ def index():
     """Return HTML"""
     return render_template('index.html')
 
-## Receivable event from client to server
-# Reserved events
+# Reserved event: Connect
 @socketio.on('connect')
 def test_connect():
     """Gets fired on any client-server connection"""
     print("MAIN: Connected:", request.sid)
-    event_manager.process_message(request.sid, """{"type": "connect"}""")
+    process_message(request.sid, """{"type": "connect"}""")
 
-
+# Reserved event: Disconnect
 @socketio.on('disconnect')
 def test_disconnect():
     """Gets fired on any client-server disconnect"""
     print('MAIN: Disconnected:', request.sid)
-    event_manager.process_message(request.sid, """{"type": "disconnect"}""")
+    process_message(request.sid, """{"type": "disconnect"}""")
 
-
-# Custom events
+# Custom event: Any message from client
 @socketio.on('message_from_client') #  Event name is crucial.
 def handle_my_custom_event(json_data):
     """Use this function to receive events from the client.
        A JSON is always sent along and it hold all necessary data."""
     #print("MAIN: Received a message and forwarding to MessageHandler.")
-    event_manager.process_message(request.sid, json_data)
-
-
+    process_message(request.sid, json_data)
 
 ## Emit events to one client as response
 def emit_event(event_name, json_data):
@@ -50,18 +46,54 @@ def emit_event(event_name, json_data):
     print("MAIN: Sending to one client as response: ",event_name)
     emit(event_name, json_data)
 
-
-
 ## Emit events to all clients as broadcast
 def emit_event_to_all_clients(event_name, json_data):
     """Use this function to emit events to all clients as broadcast.
        A JSON is always sent along and it hold all necessary data."""
     print("MAIN: Sending to all clients: ",event_name)
 
+## ALL EVENTS ARE PROCESSED HERE --------------------------------
+def process_message(session_id, json_data):
+    """Processes each message type defined in "type" in JSON."""
+    print("MODEL: Event Type:", json.loads(json_data)["type"])
+
+    data = json.loads(json_data)
+
+    if data["type"] == "echo":
+        print("MODEL:", session_id, json_data)
+
+    elif data["type"] == "connect":
+        pass
+
+    elif data["type"] == "disconnect":
+        game_session.remove_player(session_id)
+        #print("MODEL:", session_id, json_data)
+
+    elif data["type"] == "game_start":
+        print("MODEL:", session_id, json_data)
+        if game_session.check_if_player_exists(session_id):
+            print("Player already exists.")
+
+        else:
+            game_session.add_player(session_id)
+            game_session.list_players()
+
+        check_players_in_session()
+
+    elif data["type"] == "":
+        pass
+#----------------------------------------------------------------
+
+## GAME MODEL
+def check_players_in_session():
+    print("Current players:", len(game_session.players))
+
+
+
 
 # Boilerplate starts SocketIO instead of standard flask.
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host="192.168.178.23", port="5001")
 
 
 """
