cabbeedb4283561cb66cf2886f1015783f101df7
NoSkiLL <manuel_kuenstler@web.de>
2018-04-12 22:08:09 +0200

    more html markup

--- client/src/static/js/app.js
+++ client/src/static/js/app.js
@@ -4,10 +4,10 @@ import io from 'socket.io-client'
 
 window.$ = $;
 
-import Foundation from 'foundation-sites';
+// import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
+import './lib/foundation-explicit-pieces';
 
 
 $(document).foundation();
